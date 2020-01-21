f41af48881af72b90a4883b90f03186d34167bfa
FelipeOkino <felipe1okino@live.com>
2020-01-21 21:30:44 +0000

    jwt auth

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask_restful import Resource, Api
+from flask_jwt_extended import JWTManager
 
+# Importing route classes
+from routes.Login import Login
+from routes.File import File
+
+# Api configuration
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'super-secret'
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
+jwt = JWTManager(app)
+
+api = Api(app)
+
+api.add_resource(Login, '/login')
+api.add_resource(File, '/file')
 
 if __name__ == '__main__':
     app.run()
